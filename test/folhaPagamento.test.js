6e51fe078689de40a475af8224250f05793b6028
elizabethbitenc <elizabethbitencourt14@gmail.com>
2024-12-31 15:29:56 -0300

    testes

--- test/folhaPagamento.test.js
+++ test/folhaPagamento.test.js
@@ -1,1 +1,17 @@
-import { somaHorasExtras, calculaDescontos } from "./index.js";
+import { somaHorasExtras, calculaDescontos } from "../index.js"
+
+describe('Testes dos cálculos da folha de pagamento', ()=> {
+    test('Deve retornar a soma das horas extras', ()=> {
+        const esperado = 2500
+        const retornado = somaHorasExtras(2000, 500)
+    
+        expect(retornado).toBe(esperado)
+    })
+    
+    test('Deve descontar o valor do salario', ()=> {
+        const esperado = 2300
+        const retornado = calculaDescontos(2500, 200)
+    
+        expect(retornado).toBe(esperado)
+    })
+})
